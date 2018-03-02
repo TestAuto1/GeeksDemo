ec5ed8b96d2b7e92fee0006a1dd3ec0cd42b0242
moumen <moumen.gamil@vodafone.com>
2018-03-02 11:40:42 +0200

    initial commit

---
+++ Geeks/src/test/java/Test/Test11.java
@@ -0,0 +1,26 @@
+package Test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test11{
+
+	
+
+	@Test				
+	public void Dummy_test_to_pass() 
+	{									 
+	Assert.assertEquals(1,1); 		
+	}	
+	@Test				
+	 public void Dummy_test_to_fail()
+	 {								 
+	Assert.assertEquals(1,2);
+	
+	}
+	
+	
+
+}
+
+
